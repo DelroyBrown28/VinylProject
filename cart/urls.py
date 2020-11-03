6557e085c911a448ffc55250bb0e934e42e6c832
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-03 14:00:46 +0000

    can now increment and decrement quantity of item in cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from cart.views import CartView, ProductDetailView
+from cart.views import CartView, IncreaseQuantityView, ProductDetailView
 from . import views
 
 
@@ -8,6 +8,9 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.CartView.as_view(), name='summary'),
     path('shop/', views.ProductListView.as_view(), name='product-list'),
-    path('shop/<slug>/', views.ProductDetailView.as_view(), name='product-detail')
+    path('shop/<slug>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('increase-quantity/<pk>/', views.IncreaseQuantityView.as_view(), name='increase-quantity'),
+    path('decrease-quantity/<pk>/', views.DecreaseQuantityView.as_view(), name='decrease-quantity'),
+    path('remove-from-cart/<pk>/', views.RemoveFromCartView.as_view(), name='remove-from-cart'),
     
 ]
