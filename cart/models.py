0b72c90554deb4ee5ced57f77f9b332bdf970d41
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-11-09 22:55:15 +0000

    added product list delete button to staff portal

--- cart/models.py
+++ cart/models.py
@@ -53,6 +53,10 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("cart:product-detail", kwargs={"slug": self.slug})
+    
+    def get_delete_url(self):
+        return reverse("staff:product-delete", kwargs={"pk": self.pk})
+        
 
     def get_price(self):
 
